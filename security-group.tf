93ce6a2934b6fb19258732c767b1867a14636694
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2022-11-30 20:05:24 +0530

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,12 +1,32 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.41.0"
-    }
-  }
-}
+resource "aws_security_group" "allow_sg" {
+  name        = "Terraform-SG"
+  description = "Allow tls inbound traffic"
 
-provider "aws" {
-  region = "us-east-1"
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["157.35.3.5/32"]
+  }
+  
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 8082
+    to_port          = 8082
+    protocol         = "tcp"
+    cidr_blocks      = ["157.35.3.5/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Terraform-SG"
+  }
 }
